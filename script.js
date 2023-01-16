3ec4aa6d2fe825732612bb17ac5fe39375f28f29
KimberlyJane57 <kimberlythemotherofgod@gmail.com>
2023-01-16 11:40:47 -0700

    worked on date

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const currentDay = getEle
-dayjs.extend(LocalizedFormat)
-dayjs().format('L LT')
+let currentDate = dayjs();
+$('#currentDay').text(currentDate.format('	MMMM D, YYYY'));
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
